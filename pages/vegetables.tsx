21b4003fdeaebd31b9ae40586eb5421bf45a0603
Daniel Eden <dan.eden@me.com>
2021-11-24 17:17:12 +0000

    Add a page to see rankings for vegetables. Don't ask why. Don't worry about it.

---
+++ pages/vegetables.tsx
@@ -0,0 +1,24 @@
+import { PrismaClient } from "@prisma/client"
+import getLeaderboard from "../utils/getLeaderboard"
+import HomePage from "./index"
+
+export default function Vegetables({ leaderboard }) {
+  return <HomePage leaderboard={leaderboard} />
+}
+
+export async function getStaticProps() {
+  const prisma = new PrismaClient()
+  const allFighters = await getLeaderboard(prisma)
+  const leaderboard = allFighters.filter((fighter) => {
+    const regex =
+      /(potato|carrot|onion|ear of corn|pumpkin|tomato|eggplant|pepper|avocado|cucumber|garlic|leafy green|broccoli)/
+    return regex.test(fighter.label)
+  })
+
+  return {
+    props: {
+      leaderboard,
+    },
+    revalidate: 60 * 60 * 8,
+  }
+}
---
+++ utils/getLeaderboard.ts
@@ -0,0 +1,86 @@
+import { PrismaClient } from "@prisma/client"
+
+export interface EmojiWithVoteCount {
+  emoji: string
+  label: string
+  wins: number
+  losses: number
+  votes: number
+  ties: number
+  gamesPlayed: number
+  winningPercentage: number
+}
+
+const baseEmojiWithVoteCount: Omit<EmojiWithVoteCount, "label" | "emoji"> = {
+  wins: 0,
+  losses: 0,
+  gamesPlayed: 0,
+  winningPercentage: 0,
+  votes: 0,
+  ties: 0,
+}
+
+export default async function getLeaderboard(prisma: PrismaClient) {
+  const competitions = await prisma.fight.findMany({
+    select: {
+      fighterReports: { select: { fighter: true, votes: true } },
+      id: true,
+      victor: true,
+    },
+    where: {
+      tallied: true,
+    },
+  })
+
+  const leaderboard: EmojiWithVoteCount[] = Object.values(
+    competitions.reduce<{ [key: string]: EmojiWithVoteCount }>(
+      (leaderboard, poll) => {
+        const [winner, loser] = poll.fighterReports.sort(
+          (a, b) => b.votes - a.votes
+        )
+
+        if (!leaderboard.hasOwnProperty(winner.fighter.shortname)) {
+          leaderboard[winner.fighter.shortname] = {
+            label: winner.fighter.shortname,
+            emoji: winner.fighter.utf,
+            ...baseEmojiWithVoteCount,
+          }
+        }
+
+        if (!leaderboard.hasOwnProperty(loser.fighter.shortname)) {
+          leaderboard[loser.fighter.shortname] = {
+            label: loser.fighter.shortname,
+            emoji: loser.fighter.utf,
+            ...baseEmojiWithVoteCount,
+          }
+        }
+
+        leaderboard[winner.fighter.shortname].votes += winner.votes
+        leaderboard[loser.fighter.shortname].votes += loser.votes
+
+        leaderboard[winner.fighter.shortname].gamesPlayed += 1
+        leaderboard[loser.fighter.shortname].gamesPlayed += 1
+
+        if (winner.votes === loser.votes) {
+          leaderboard[winner.fighter.shortname].ties += 1
+          leaderboard[loser.fighter.shortname].ties += 1
+        } else {
+          leaderboard[winner.fighter.shortname].wins += 1
+          leaderboard[loser.fighter.shortname].losses += 1
+        }
+
+        return leaderboard
+      },
+      {}
+    )
+  )
+    .map((fighter) => {
+      const { wins, ties } = fighter
+      fighter.winningPercentage = (2 * wins + ties) / (2 * competitions.length)
+
+      return fighter
+    })
+    .sort((a, b) => b.winningPercentage - a.winningPercentage)
+
+  return leaderboard
+}
